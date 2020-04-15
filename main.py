0bd003d2c9eeb03f879a2df852cd36bfd9f231a8
Kamieljv <kamiel.verhelst@gmail.com>
2020-04-15 20:26:18 +0200

    Started attempt to load images (np subtract not working yet)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from mxnet import gluon, np, npx
+##
+from mxnet import gluon, npx, np
 npx.set_np()
 import os, shutil, zipfile
-import zipfile
 from git import Repo
 import skimage.io as io
 import matplotlib.pyplot as plt
@@ -32,8 +32,8 @@ for name in ['UCMImages', 'README.md', 'UCMerced_LandUse.zip']:
             os.remove(file)
         print('Removed: ' + file)
 
-UCM_images_path = "Images/"
-Multilabels_path = "LandUse_Multilabeled.txt"
+UCM_images_path = os.path.join(data_folder, "Images/")
+Multilabels_path = os.path.join(data_folder, "LandUse_Multilabeled.txt")
 
 ##
 
@@ -45,9 +45,9 @@ Multilabels_path = "LandUse_Multilabeled.txt"
 
 ##
 # ============== DEFINING THE SINGLELABEL DATASET CLASS ===================
-class Dataset_Siglelabel(gluon.data.Dataset):
+class Dataset_Singlelabel(gluon.data.Dataset):
     def __init__(self, img_folder, label_file):
-        super(Dataset_Siglelabel, self).__init__()
+        super(Dataset_Singlelabel, self).__init__()
         self.rgb_mean = np.array([0.485, 0.456, 0.406])
         self.rgb_std = np.array([0.229, 0.224, 0.225])
 
@@ -78,16 +78,24 @@ class Dataset_Multilabel(gluon.data.Dataset):
         super(Dataset_Multilabel, self).__init__()
         self.rgb_mean = np.array([0.485, 0.456, 0.406])
         self.rgb_std = np.array([0.229, 0.224, 0.225])
+        self.imgs = []
+        self.labels = []
+
+        # Load labels as dataframe
+        label_df = pd.read_csv(label_file, sep="\t")
 
         # Define self.images
-        sub_folders = os.listdir(img_folder)
-        image_list = [os.listdir(os.path.join(img_folder, folder)) for folder in sub_folders]
-        self.imgs = sum(image_list, [])
-        self.imgs.sort()
+        for sub_folder in os.listdir(img_folder):
+            print("Processing {} images".format(sub_folder))
+            for file in os.listdir(os.path.join(img_folder, sub_folder)):
+                img = io.imread(os.path.join(img_folder, sub_folder, file)) / 255
+                # normalize the image with mean and stdev
+                print(type(img))
+                img_norm = (img.astype('float32') - np.tile(self.rgb_mean, (img.shape[0], img.shape[1], 1))) / np.tile(self.rgb_std, (img.shape[0], img.shape[1], 1))
+                self.imgs.append(img_norm)
+                label = label_df.loc[label_df['IMAGE\LABEL'] == name.split('.')[0]].values.flatten().tolist()[1:]
+                self.labels.append(label)
 
-        # Define self.labels
-        label_df = pd.read_csv(label_file, sep="\t")
-        self.labels = [label_df.loc[label_df['IMAGE\LABEL'] == name.split('.')[0]].values.flatten().tolist()[1:] for name in label_df['IMAGE\LABEL']]
 
     def __getitem__(self, idx):
         #__getitem__ asks for the sample number idx. Since we pre-loaded the images
@@ -98,4 +106,6 @@ class Dataset_Multilabel(gluon.data.Dataset):
 
     def __len__(self):
         return len(self.imgs)
+
+multilabel = Dataset_Multilabel(UCM_images_path, Multilabels_path)
 ##
