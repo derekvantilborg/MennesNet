215a7a7ff92a0e10bad6c6eb01643988a3b4e992
Kamieljv <kamiel.verhelst@gmail.com>
2020-04-16 00:07:55 +0200

    Implemented Single Dataset; started dataloader implementation; (FIX ONE_HOT)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-##
+## =============== PERFORMING IMPORTS ==============
+import mxnet
 from mxnet import gluon, npx
 import numpy as np
 npx.set_np()
@@ -8,7 +9,7 @@ import skimage.io as io
 import matplotlib.pyplot as plt
 import pandas as pd
 
-##
+## ============ LOADING THE DATA ===============
 
 data_folder = 'ucmdata'
 
@@ -36,31 +37,34 @@ for name in ['UCMImages', 'README.md', 'UCMerced_LandUse.zip']:
 UCM_images_path = os.path.join(data_folder, "Images/")
 Multilabels_path = os.path.join(data_folder, "LandUse_Multilabeled.txt")
 
-##
+## ============= PLOTTING A TEST IMAGE ==============================
 
-# # plotting a test image
 # image = io.imread('Images/golfcourse/golfcourse60.tif')
 # f = plt.figure()
 # plt.imshow(image)
 # plt.show()
 
-##
-# ============== DEFINING THE SINGLELABEL DATASET CLASS ===================
+## ============== DEFINING THE SINGLELABEL DATASET CLASS ===================
 class Dataset_Singlelabel(gluon.data.Dataset):
-    def __init__(self, img_folder, label_file):
+    def __init__(self, img_folder, n_classes = 21):
         super(Dataset_Singlelabel, self).__init__()
         self.rgb_mean = np.array([0.485, 0.456, 0.406])
         self.rgb_std = np.array([0.229, 0.224, 0.225])
+        self.imgs = []
+        self.labels = []
 
         # Define self.images
-        sub_folders = os.listdir(img_folder)
-        image_list = [os.listdir(os.path.join(img_folder, folder)) for folder in sub_folders]
-        self.imgs = sum(image_list, [])
-        self.imgs.sort()
-
-        # Define self.labels
-        label_df = pd.read_csv(label_file, sep="\t")
-        self.labels = [label_df.loc[label_df['IMAGE\LABEL'] == name.split('.')[0]].values.flatten().tolist()[1:] for name in label_df['IMAGE\LABEL']]
+        for sub_folder in os.listdir(img_folder):
+            print("Processing {} images".format(sub_folder))
+            for file in os.listdir(os.path.join(img_folder, sub_folder)):
+                img = io.imread(os.path.join(img_folder, sub_folder, file)) / 255
+                # normalize the image with mean and stdev
+                img_norm = (img.astype('float32') - np.tile(self.rgb_mean, (img.shape[0], img.shape[1], 1))) / np.tile(
+                    self.rgb_std, (img.shape[0], img.shape[1], 1))
+                self.imgs.append(img_norm)
+                self.labels.append(file.split('.')[0][:-2])
+        #one-hot encoding of labels
+        self.labels = mxnet.ndarray.one_hot(self.labels, n_classes)
 
     def __getitem__(self, idx):
         #__getitem__ asks for the sample number idx. Since we pre-loaded the images
@@ -71,6 +75,24 @@ class Dataset_Singlelabel(gluon.data.Dataset):
 
     def __len__(self):
         return len(self.imgs)
+
+## =============== DEFINING DATA LOADERS ==================
+
+Dataset_Single = Dataset_Singlelabel(UCM_images_path, Multilabels_path)
+
+from multiprocessing import cpu_count
+CPU_COUNT = cpu_count()
+print("Number of available CPUs: " + str(CPU_COUNT))
+
+data_loader = gluon.data.DataLoader(Dataset_Single,
+                                    batch_size=5,
+                                    shuffle=True,
+                                    last_batch='discard',
+                                    num_workers=1)
+
+for X_batch, y_batch in data_loader:
+    print("X_batch has shape {}, and y_batch has shape {}".format(X_batch.shape, y_batch.shape))
+
 
 ##
 # ============== DEFINING THE MULTILABEL DATASET CLASS ===================
